6a340f87b4ec808d392597860334fc538d0ac804
kianz20 <kianja02@gmail.com>
2024-08-31 01:03:24 +1200

    Fix sonarcloud complaints
    #17
    Destructure props in singleProduct.tsx
    Combine imports

--- client/src/components/SingleProduct.tsx
+++ client/src/components/SingleProduct.tsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProductDetails } from "../models";
 import styles from "../styles/Products.module.css";
-import { useState } from "react";
 
-const SingleProduct: React.FC<ProductDetails> = (product: ProductDetails) => {
+const SingleProduct: React.FC<ProductDetails> = ({
+	name,
+	price,
+	description,
+}) => {
 	const [isLoaded, setIsLoaded] = useState(false);
 
 	return (
 		<div className={styles.singleProduct}>
-			<div className={styles.singleProductText}>{product.name}</div>
-			<div className={styles.singleProductText}>${product.price}</div>
+			<div className={styles.singleProductText}>{name}</div>
+			<div className={styles.singleProductText}>${price}</div>
 			<div className={styles.imageContainer}>
 				<img
 					onLoad={() => setIsLoaded(true)}
 					src="https://picsum.photos/600/500"
+					alt={`Image of ${name}`}
 				/>
 				{!isLoaded && <div className={styles.skeletonImg}></div>}
 			</div>
-			<div className={styles.singleProductText}>
-				{product.description}
-			</div>
+			<div className={styles.singleProductText}>{description}</div>
 		</div>
 	);
 };

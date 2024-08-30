5baca8b7737a0c3abf30cfaf286b59fa1f3cc78d
Biscuitmunch <biscuitmunch0@gmail.com>
2024-08-30 23:57:23 +1200

    feat: product grid improvements

--- client/src/components/SingleProduct.tsx
+++ client/src/components/SingleProduct.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { ProductDetails } from "../models";
 import styles from "../styles/Products.module.css";
+import { useState } from "react";
 
 const SingleProduct: React.FC<ProductDetails> = (product: ProductDetails) => {
+	const [isLoaded, setIsLoaded] = useState(false);
+
 	return (
 		<div className={styles.singleProduct}>
-			<p>{product.name}</p>
-			<p>{product.details}</p>
-			<p>{product.price}</p>
+			<div className={styles.singleProductText}>{product.name}</div>
+			<div className={styles.singleProductText}>${product.price}</div>
+			<div className={styles.imageContainer}>
+				<img
+					onLoad={() => setIsLoaded(true)}
+					src="https://picsum.photos/600/500"
+				/>
+				{!isLoaded && <div className={styles.skeletonImg}></div>}
+			</div>
+			<div className={styles.singleProductText}>
+				{product.description}
+			</div>
 		</div>
 	);
 };

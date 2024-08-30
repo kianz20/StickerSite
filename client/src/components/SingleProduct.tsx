55efa52ceac27421e8b070ef68c8e7cc6861f44b
Biscuitmunch <biscuitmunch0@gmail.com>
2024-08-30 23:52:25 +1200

    feat: improved products page

--- client/src/components/SingleProduct.tsx
+++ client/src/components/SingleProduct.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { productDetails } from "../models";
 import styles from "../styling/Products.module.css";
+import { useState } from "react";
 
 const SingleProduct: React.FC<productDetails> = (product: productDetails) => {
+	const [isLoaded, setIsLoaded] = useState(false);
+
 	return (
 		<div className={styles.singleProduct}>
-            <p>{product.name}</p>
-            <p>{product.details}</p>
-            <p>{product.price}</p>
-        </div>
+			<div className={styles.singleProductText}>{product.name}</div>
+			<div className={styles.singleProductText}>${product.price}</div>
+			<div className={styles.imageContainer}>
+				<img
+					onLoad={() => setIsLoaded(true)}
+					src="https://picsum.photos/600/500"
+				/>
+				{!isLoaded && <div className={styles.skeletonImg}></div>}
+			</div>
+			<div className={styles.singleProductText}>
+				{product.description}
+			</div>
+		</div>
 	);
 };
 

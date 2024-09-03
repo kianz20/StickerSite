9f37cedd90098ac17b149d8e0c05c55ba25da914
kianz20 <kianja02@gmail.com>
2024-09-03 20:21:04 +1200

    Added filter and sort to products page
    Filter and sort functions added to products grid page
    Changed some styling for search bar

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -7,23 +7,31 @@ import {
 	SearchBar,
 	SingleProduct,
 	ThemedButton,
+	ThemedInput,
 } from "../components/";
 import { useAuth } from "../hooks";
 import { ProductDetails } from "../models";
 import styles from "../styles/Products.module.css";
 
 interface FilterOptions {
-	minPrice: number;
-	maxPrice: number;
-	category: string;
-	franchise: string;
+	minPrice?: number;
+	maxPrice?: number;
+	category?: string[];
+	franchise?: string[];
 }
 
 const Products = (): JSX.Element => {
-	const [products, setProducts] = useState<ProductDetails[]>();
-	const [filters, setFilters] = useState<FilterOptions>();
-	const [sortBy, setSortBy] = useState("mostPopular");
 	const { userToken } = useAuth();
+	const categoryList = ["Stickers", "Frames", "Pins and Badges", "misc"];
+	const [products, setProducts] = useState<ProductDetails[]>();
+	const [filters, setFilters] = useState<FilterOptions>({
+		category: categoryList,
+	});
+	const [minPrice, setMinPrice] = useState<string>("");
+	const [maxPrice, setMaxPrice] = useState<string>("");
+
+	const [filtersOpen, setFiltersOpen] = useState(false);
+	const [sortBy, setSortBy] = useState("mostPopular");
 
 	const handleGetAllProducts = async () => {
 		if (userToken) {
@@ -32,19 +40,54 @@ const Products = (): JSX.Element => {
 		}
 	};
 
+	const applyFilters = () => {
+		setFilters((prevFilters) => ({
+			...prevFilters,
+			minPrice: minPrice ? parseFloat(minPrice) : undefined,
+			maxPrice: maxPrice ? parseFloat(maxPrice) : undefined,
+		}));
+		console.log(filters);
+	};
+
 	const handleSortByChange = (event: SelectChangeEvent<string>) => {
 		const { value } = event.target;
 		setSortBy(value);
 	};
 
-	const sortedProducts = products?.slice().sort((a, b) => {
-		if (sortBy === "priceAsc") {
-			return a.price - b.price;
-		} else if (sortBy === "priceDesc") {
-			return b.price - a.price;
-		}
-		return 0;
-	});
+	const toggleFilterOpen = () => {
+		setFiltersOpen(!filtersOpen);
+	};
+
+	const sortedProducts = products
+		?.filter((product) => {
+			const inCategory = filters?.category?.includes(product.category);
+			const withinPriceRange =
+				(filters.minPrice === undefined || product.price >= filters.minPrice) &&
+				(filters.maxPrice === undefined || product.price <= filters.maxPrice);
+
+			return inCategory && withinPriceRange;
+		})
+		.slice()
+		.sort((a, b) => {
+			if (sortBy === "priceAsc") {
+				return a.price - b.price;
+			} else if (sortBy === "priceDesc") {
+				return b.price - a.price;
+			}
+			return 0;
+		});
+
+	const handleCheckboxChange = (category: string) => {
+		setFilters((prevFilters) => {
+			const currentCategories = prevFilters.category || [];
+
+			const newCategories = currentCategories.includes(category)
+				? currentCategories.filter((cat) => cat !== category)
+				: [...currentCategories, category];
+
+			return { ...prevFilters, category: newCategories };
+		});
+	};
 
 	useEffect(() => {
 		handleGetAllProducts();
@@ -56,10 +99,48 @@ const Products = (): JSX.Element => {
 			<SearchBar />
 
 			<div className={styles.filterProductsBar}>
-				<Typography> {products?.length} Items</Typography>
-				<ThemedButton variant="text">
+				<Typography> {sortedProducts?.length} Items</Typography>
+				<ThemedButton variant="text" onClick={toggleFilterOpen}>
 					<FilterAltIcon className={styles.filterIcon} />
 				</ThemedButton>
+				{filtersOpen && (
+					<div className={styles.filterMenu}>
+						<Typography className={styles.filtersTitle}>Filters</Typography>
+						<div className={styles.priceFilterContainer}>
+							<Typography className={styles.filtersTitle}>Price:</Typography>
+							<ThemedInput
+								inputProps={{ maxLength: 4 }}
+								className={styles.priceSelector}
+								size="small"
+								value={minPrice}
+								onChange={(e) => setMinPrice(e.target.value)}
+							/>
+							<Typography> - </Typography>
+							<ThemedInput
+								inputProps={{ maxLength: 4 }}
+								className={styles.priceSelector}
+								size="small"
+								value={maxPrice}
+								onChange={(e) => setMaxPrice(e.target.value)}
+							/>
+							<ThemedButton text="Apply" onClick={applyFilters} />
+						</div>
+						<div className={styles.categorySelector}>
+							{categoryList.map((category) => (
+								<div key={category}>
+									<label>
+										<input
+											type="checkbox"
+											checked={filters.category?.includes(category)}
+											onChange={() => handleCheckboxChange(category)}
+										/>
+										{category}
+									</label>
+								</div>
+							))}
+						</div>
+					</div>
+				)}
 				<Typography className={styles.sortByText}> Sort By: </Typography>
 				<Select
 					variant="standard"
